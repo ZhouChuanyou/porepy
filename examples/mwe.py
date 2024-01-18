f610c10ba14921c1cc4160b3d36ebb55f4f25287
Omar Duran <omaryesiduran@gmail.com>
2024-01-18 14:16:39 +0100

    MAINT: Ongoing work with mwe

--- examples/mwe.py
+++ examples/mwe.py
@@ -2,41 +2,21 @@
 import porepy as pp
 import numpy as np
 from porepy.models.fluid_mass_balance import SinglePhaseFlow
-
 from porepy.models.fluid_mass_balance import BoundaryConditionsSinglePhaseFlow
 from porepy.applications.md_grids.domains import nd_cube_domain
-
 
 class ModifiedGeometry:
     def set_domain(self) -> None:
         """Defining a two-dimensional square domain with sidelength 2."""
-        size = self.solid.convert_units(2, "m")
-        self._domain = nd_cube_domain(2, size)
-
-    def set_fractures(self) -> None:
-        """Setting a diagonal fracture"""
-        frac_1_points = self.solid.convert_units(
-            np.array([[0.2, 1.8], [0.2, 1.8]]), "m"
-        )
-        frac_1 = pp.LineFracture(frac_1_points)
-        self._fractures = [frac_1]
+        size = self.solid.convert_units(1, "m")
+        dimension = 2
+        self._domain = nd_cube_domain(dimension, size)
 
     def grid_type(self) -> str:
-        """Choosing the grid type for our domain.
-
-        As we have a diagonal fracture we cannot use a cartesian grid.
-        Cartesian grid is the default grid type, and we therefore override this method to assign simplex instead.
-
-        """
         return self.params.get("grid_type", "simplex")
 
     def meshing_arguments(self) -> dict:
-        """Meshing arguments for md-grid creation.
-
-        Here we determine the cell size.
-
-        """
-        cell_size = self.solid.convert_units(0.25, "m")
+        cell_size = self.solid.convert_units(0.01, "m")
         mesh_args: dict[str, float] = {"cell_size": cell_size}
         return mesh_args
 
@@ -48,25 +28,73 @@ class SinglePhaseFlowGeometry(
     ...
 
 
+
+# Pressure fuction at boundary
+def p_D(xv):
+    x,y,z = xv
+    val = (1 - x)*x*(y**2)*np.sin(2*np.pi*x)
+    return val
+
+def f_rhs(xv):
+    x,y,z = xv
+    val = 4*np.pi*(-1 + 2*x)*(y**2)*np.cos(2*np.pi*x) + 2*((y**2) + (-1 + x)*x*(1 - 2*(np.pi**2)*(y**2)))*np.sin(2*np.pi*x)
+    return val
+
+def qn_at_top_bc(xv):
+    x,y,z = xv
+    val = -2*(1 - x)*x*y*np.sin(2*np.pi*x)
+    return val
+
 class ModifiedBC(BoundaryConditionsSinglePhaseFlow):
+
     def bc_type_darcy_flux(self, sd: pp.Grid) -> pp.BoundaryCondition:
         """Assign dirichlet to the west and east boundaries. The rest are Neumann by default."""
         bounds = self.domain_boundary_sides(sd)
-        bc = pp.BoundaryCondition(sd, bounds.west + bounds.east, "dir")
+        bc = pp.BoundaryCondition(sd, bounds.all_bf, "dir")
         return bc
 
     def bc_values_pressure(self, boundary_grid: pp.BoundaryGrid) -> np.ndarray:
         """Zero bc value on top and bottom, 5 on west side, 2 on east side."""
         bounds = self.domain_boundary_sides(boundary_grid)
         values = np.zeros(boundary_grid.num_cells)
-        # See section on scaling for explanation of the conversion.
-        values[bounds.west] = self.fluid.convert_units(5, "Pa")
-        values[bounds.east] = self.fluid.convert_units(2, "Pa")
+        xc = boundary_grid.cell_centers.T
+        values[bounds.all_bf] = np.array(list(map(p_D, xc)))
         return values
 
+    # def bc_values_fluid_flux(self, sd: pp.Grid) -> pp.BoundaryCondition:
+    #     """Assign dirichlet to the west and east boundaries. The rest are Neumann by default."""
+    #     bounds = self.domain_boundary_sides(sd)
+    #     bc = pp.BoundaryCondition(sd, bounds.west, "dir")
+    #     return bc
+
+
+class ModifiedSource:
+    def fluid_source(self, subdomains: list[pp.Grid]) -> pp.ad.Operator:
+        """Assign unitary fracture source"""
+        # Retrieve internal sources (jump in mortar fluxes) from the base class
+        internal_sources: pp.ad.Operator = super().fluid_source(subdomains)
+
+        # Retrieve external (integrated) sources from the exact solution.
+        values = []
+
+        for sd in subdomains:
+            if sd.dim == self.mdg.dim_max():
+                xc = sd.cell_centers.T
+                values = sd.cell_volumes * np.array(list(map(f_rhs, xc)))
+            else:
+                values.append(np.zeros(sd.num_cells))
+
+        external_sources = pp.wrap_as_dense_ad_array(np.hstack(values))
+
+        # Add up both contributions
+        source = internal_sources + external_sources
+        source.set_name("fluid sources")
+
+        return source
 
 class SinglePhaseFlowGeometryBC(
     ModifiedGeometry,
+    ModifiedSource,
     ModifiedBC,
     SinglePhaseFlow):
     """Adding both geometry and modified boundary conditions to the default model."""
@@ -74,12 +102,14 @@ class SinglePhaseFlowGeometryBC(
 
 
 params = {}
-fluid_constants = pp.FluidConstants({"viscosity": 0.1, "density": 0.2})
-solid_constants = pp.SolidConstants({"permeability": 0.5, "porosity": 0.25})
+fluid_constants = pp.FluidConstants({"viscosity": 1.0, "density": 1.0})
+solid_constants = pp.SolidConstants({"permeability": 1.0, "porosity": 0.0})
 material_constants = {"fluid": fluid_constants, "solid": solid_constants}
 params = {"material_constants": material_constants}
 
 
 model = SinglePhaseFlowGeometryBC(params)
 pp.run_time_dependent_model(model, params)
-pp.plot_grid(model.mdg, "pressure", figsize=(10, 8), linewidth=0.25, title="Pressure distribution", plot_2d=True)
+model.exporter.write_vtu([model.pressure_variable])
+
+# pp.plot_grid(model.mdg, "pressure", figsize=(10, 8), linewidth=0.25, title="Pressure distribution", plot_2d=True)
